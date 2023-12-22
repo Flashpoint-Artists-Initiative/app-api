7ab793229208d882c2966fdd63bdfa84bb1cfac1
Syzgyn <git@syzgyn.com>
2023-12-22 13:57:47 -0500

    Hopefully fix trusted hosts on aws dev

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -12,6 +12,7 @@ use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\URL;
 
 class AuthServiceProvider extends ServiceProvider
@@ -46,5 +47,7 @@ class AuthServiceProvider extends ServiceProvider
                 ]
             );
         });
+
+        Gate::define('viewApiDocs', fn () => true);
     }
 }
