cd46f6fc03f6e1e0b288e40f566ae544bdc52d4a
Syzgyn <git@syzgyn.com>
2024-01-02 15:34:23 -0500

    Update requests and policies

---
+++ app/Http/Requests/TicketTypeRequest.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+use Orion\Http\Requests\Request;
+
+class UpdateTicketTypeRequest extends Request
+{
+}
--- app/Policies/PurchasedTicketPolicy.php
+++ app/Policies/PurchasedTicketPolicy.php
@@ -1,66 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Policies;
 
-use App\Models\PurchasedTicket;
-use App\Models\User;
-use Illuminate\Auth\Access\Response;
-
-class PurchasedTicketPolicy
+class PurchasedTicketPolicy extends AbstractModelPolicy
 {
-    /**
-     * Determine whether the user can view any models.
-     */
-    public function viewAny(User $user): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can view the model.
-     */
-    public function view(User $user, PurchasedTicket $purchasedTicket): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can create models.
-     */
-    public function create(User $user): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can update the model.
-     */
-    public function update(User $user, PurchasedTicket $purchasedTicket): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can delete the model.
-     */
-    public function delete(User $user, PurchasedTicket $purchasedTicket): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, PurchasedTicket $purchasedTicket): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, PurchasedTicket $purchasedTicket): bool
-    {
-        //
-    }
+    protected string $prefix = 'purchasedTickets';
 }
