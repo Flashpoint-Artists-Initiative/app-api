d7953c67773b694fc5529518c2c94a7e22b4fb3e
Syzgyn <git@syzgyn.com>
2024-01-25 13:15:51 -0500

    Switch to existing logging

--- app/Http/Controllers/Api/StripeWebhookController.php
+++ app/Http/Controllers/Api/StripeWebhookController.php
@@ -8,9 +8,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StripeWebhookRequest;
 use App\Services\CheckoutService;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Monolog\Handler\StreamHandler;
-use Monolog\Level;
-use Monolog\Logger;
+use Illuminate\Support\Facades\Log;
 use Stripe\Checkout\Session;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
@@ -31,20 +29,17 @@ class StripeWebhookController extends Controller
 
     public function checkoutSessionCompleted()
     {
-        $logger = new Logger('stripe-webook');
-        $logger->pushHandler(new StreamHandler('php://stderr', Level::Info));
-
         /** @var Session $session */
         $session = request('event')->data->object;
 
         try {
             $this->checkoutService->resolveCompletedCheckoutSession($session);
         } catch (ModelNotFoundException $e) { // When there is no cart found
-            $logger->info('Stripe webhook failed', ['id' => $session->id, 'exception' => $e]);
+            Log::channel('stderr')->info('Stripe webhook failed', ['id' => $session->id, 'exception' => $e]);
 
             return response()->json(status: 204);
         } catch (HttpException $e) { // abort() exceptions
-            $logger->info('Stripe webhook failed', ['id' => $session->id, 'exception' => $e]);
+            Log::channel('stderr')->info('Stripe webhook failed', ['id' => $session->id, 'exception' => $e]);
 
             return response()->json(status: 204);
         }
