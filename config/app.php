cbefee3d48a6542b8918125f4dfd7a9b8a5c11e5
Syzgyn <git@syzgyn.com>
2025-02-18 16:33:22 -0500

    Create app and admin panels

--- config/app.php
+++ config/app.php
@@ -168,6 +168,8 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\Filament\AppPanelProvider::class,
+        App\Providers\Filament\AdminPanelProvider::class,
         App\Providers\RouteServiceProvider::class,
     ])->toArray(),
 
