439fc448b8b52c2e7f9a897eae376526796acd88
Syzgyn <git@syzgyn.com>
2025-04-13 16:09:49 -0400

    Fix broken routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -30,12 +30,12 @@ class RouteServiceProvider extends ServiceProvider
         //     return Limit::perMinute(100)->by($request->user()?->id ?: $request->ip());
         // });
 
-        // $this->routes(function () {
+        $this->routes(function () {
         //     Route::middleware('api')
         //         ->prefix('api/v1')
         //         ->group(base_path('routes/api/v1.php'));
-        //     Route::group([], base_path('routes/web.php'));
-        // });
+            Route::group([], base_path('routes/web.php'));
+        });
 
         // Web routes happen through Filament, check out app/Providers/Filament/*
     }
