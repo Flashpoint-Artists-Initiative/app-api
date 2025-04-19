9c7d74e64076aceb7e64604787243fd25e891880
Syzgyn <git@syzgyn.com>
2025-04-19 16:53:01 -0400

    Update Privacy Policy & Terms of Service to use config variables

--- resources/views/pages/privacy-policy.blade.php
+++ resources/views/pages/privacy-policy.blade.php
@@ -1,151 +1,153 @@
-<h1 class="text-3xl">POTION Privacy Policy</h1>
-<div class="body2">Last Modified: March 5th, 2025</div>
-<h2>
-    Purpose
-</h2>
-<p>
-    The purpose of this privacy policy (this "Privacy Policy") is to inform users of our Site of the following:
-</p>
-<ol>
-    <li>
-    The personal data we will collect
+<div class="prose dark:prose-invert max-w-none">
+    <h1 class="text-3xl">POTION Privacy Policy</h1>
+    <div class="body2">Last Modified: March 5th, 2025</div>
+    <h2>
+        Purpose
+    </h2>
+    <p>
+        The purpose of this privacy policy (this "Privacy Policy") is to inform users of our Site of the following:
+    </p>
+    <ol>
+        <li>
+        The personal data we will collect
+        </li>
+        <li>
+        Use of collected data
+        </li>
+        <li>
+            Who has access to the data collected
+        </li>
+        <li>
+            The rights of Site users
+        </li>
+        <li>
+            The Site's cookie policy
+        </li>
+    </ol>
+    <p>
+        This Privacy Policy applies in addition to the terms and conditions of our Site.
+    </p>
+    <h2>Consent</h2>
+    <div>By using our Site users agree that they consent to:</div>
+    <ol class="list-decimal">
+        <li>
+        <div>The conditions set out in this Privacy Policy</div>
     </li>
     <li>
-    Use of collected data
+        <div>
+            The collection, use, and retention of the data listed in this Privacy Policy.
+        </div>
     </li>
-    <li>
-        Who has access to the data collected
-    </li>
-    <li>
-        The rights of Site users
-    </li>
-    <li>
-        The Site's cookie policy
-    </li>
-</ol>
-<p>
-    This Privacy Policy applies in addition to the terms and conditions of our Site.
-</p>
-<h2>Consent</h2>
-<div>By using our Site users agree that they consent to:</div>
-<ol class="list-decimal">
-    <li>
-    <div>The conditions set out in this Privacy Policy</div>
-</li>
-<li>
-    <div>
-        The collection, use, and retention of the data listed in this Privacy Policy.
-    </div>
-</li>
-</ol>
-<h2>Personal Data We Collect</h2>
-<h3>Account Information</h3>
-<div>We collect the following data when you create an account:</div>
-<ol class="list-decimal">
-    <li>
-        Legal Name
-    </li>
-    <li>
-        Email Address
-    </li>
-    <li>
-        Birthday
-    </li>
-</ol>
-<h3>Order Information</h3>
-<p>
-    When purchasing a ticket, we will collect certain information, including your name,
-    email address, and details of the tickets you purchased.
-</p>
-<h3>Other Information</h3>
-<p>
-    We will retain any information submitted via forms on the website. Any submitted media
-    will be stored in a secure folder.
-</p>
-<h3>Usage Data</h3>
-<p>
-    When interacting with the website, we may collect additional information based on your
-    activity such as IP Address, User-Agent, Browser, Operating System, Device
-    Information, Referral URL, Links clicked, and actions taken.
-</p>
-<h2>How We Use Personal Data</h2>
-<p>
-    Your personal account information is used to verify your identity when entering the
-    event for the first time. Your email address and display name are used for
-    communication purposes.
-</p>
-<h2>Who We Share Personal Data With</h2>
-<p>
-    Your legal name and birthday are shared with event volunteers and div office vendors
-    working the gate during the event. Your display name and email address are shared with
-    necessary event leadership and volunteers. No other user information is shared with
-    any other third parties.
-</p>
-<p>
-    We will never sell your information or use it in any way that is unrelated to Alchemy
-    or other events managed by Flashpoint Artists Initiative, except in the following
-    cases:
-</p>
-<ol>
-    <li>
-        If the law requires it
-    </li>
-    <li>
-        If it is required for any legal proceeding
-    </li>
-    <li>
-        To prove or protect our legal rights
-    </li>
-</ol>
-<h2>How Long We Store Personal Data</h2>
-<p>
-    We store the information we collect about you for as long as is necessary for the
-    purpose(s) for which we originally collected it. We may retain certain information for
-    legitimate business purposes or as required by law.
-</p>
-<h2>Children</h2>
-<p>
-    The minimum age to use our website is 18 years of age. We do not knowingly collect or
-    use personal data from children under 18 years of age. If we learn that we have
-    collected personal data from a child under 18 years of age, the personal data will be
-    deleted as soon as possible. If a child under 18 years of age has provided us with
-    personal data their parent or guardian may contact us at privacy@alchemyburn.com
-</p>
-<h2>
-    How to Access, Modify, Delete, or Challenge the Data Collected
-</h2>
-<p>
-    If you would like to know if we have collected your personal data, how we have used
-    your personal data, if we have disclosed your personal data and to who we disclosed
-    your personal data, or if you would like your data to be deleted or modified in any
-    way, please email privacy@alchemyburn.com
-</p>
-<p>
-    User accounts can be deleted by the user. Upon account deletion we may retain certain
-    information for legitimate business purposes or as required by law.
-</p>
-<h2>Do Not Track Notice</h2>
-<p>
-    Do Not Track ("DNT") is a privacy preference that you can set in certain web browsers.
-    We do not track the users of our Site over time and across third party websites and
-    therefore do not respond to browser-initiated DNT signals.
-</p>
-<h2>Cookie Policy</h2>
-<p>
-    A cookie is a small file, stored on a user's hard drive by a website. Its purpose is
-    to collect data relating to the user's browsing habits. You can choose to be notified
-    each time a cookie is transmitted. You can also choose to disable cookies entirely in
-    your internet browser, but this may decrease the quality of your user experience.
-</p>
-<p>
-    We use cookies to remember your identity while using the site.
-</p>
-<h2>Modifications</h2>
-<p>
-    This Privacy Policy may be amended from time to time in order to maintain compliance
-    with the law and to reflect any changes to our data collection process. When we amend
-    this Privacy Policy we will update the "Effective Date" at the top of this Privacy
-    Policy. We recommend that our users periodically review our Privacy Policy to ensure
-    that they are notified of any updates. If necessary, we may notify users by email of
-    changes to this Privacy Policy.
-</p>
+    </ol>
+    <h2>Personal Data We Collect</h2>
+    <h3>Account Information</h3>
+    <div>We collect the following data when you create an account:</div>
+    <ol class="list-decimal">
+        <li>
+            Legal Name
+        </li>
+        <li>
+            Email Address
+        </li>
+        <li>
+            Birthday
+        </li>
+    </ol>
+    <h3>Order Information</h3>
+    <p>
+        When purchasing a ticket, we will collect certain information, including your name,
+        email address, and details of the tickets you purchased.
+    </p>
+    <h3>Other Information</h3>
+    <p>
+        We will retain any information submitted via forms on the website. Any submitted media
+        will be stored in a secure folder.
+    </p>
+    <h3>Usage Data</h3>
+    <p>
+        When interacting with the website, we may collect additional information based on your
+        activity such as IP Address, User-Agent, Browser, Operating System, Device
+        Information, Referral URL, Links clicked, and actions taken.
+    </p>
+    <h2>How We Use Personal Data</h2>
+    <p>
+        Your personal account information is used to verify your identity when entering the
+        event for the first time. Your email address and display name are used for
+        communication purposes.
+    </p>
+    <h2>Who We Share Personal Data With</h2>
+    <p>
+        Your legal name and birthday are shared with event volunteers and div office vendors
+        working the gate during the event. Your display name and email address are shared with
+        necessary event leadership and volunteers. No other user information is shared with
+        any other third parties.
+    </p>
+    <p>
+        We will never sell your information or use it in any way that is unrelated to Alchemy
+        or other events managed by {{ config('app.copyrightOrg') }}, except in the following
+        cases:
+    </p>
+    <ol>
+        <li>
+            If the law requires it
+        </li>
+        <li>
+            If it is required for any legal proceeding
+        </li>
+        <li>
+            To prove or protect our legal rights
+        </li>
+    </ol>
+    <h2>How Long We Store Personal Data</h2>
+    <p>
+        We store the information we collect about you for as long as is necessary for the
+        purpose(s) for which we originally collected it. We may retain certain information for
+        legitimate business purposes or as required by law.
+    </p>
+    <h2>Children</h2>
+    <p>
+        The minimum age to use our website is 18 years of age. We do not knowingly collect or
+        use personal data from children under 18 years of age. If we learn that we have
+        collected personal data from a child under 18 years of age, the personal data will be
+        deleted as soon as possible. If a child under 18 years of age has provided us with
+        personal data their parent or guardian may contact us at {{ config('mail.helpAddress') }}
+    </p>
+    <h2>
+        How to Access, Modify, Delete, or Challenge the Data Collected
+    </h2>
+    <p>
+        If you would like to know if we have collected your personal data, how we have used
+        your personal data, if we have disclosed your personal data and to who we disclosed
+        your personal data, or if you would like your data to be deleted or modified in any
+        way, please email {{ config('mail.helpAddress') }}
+    </p>
+    <p>
+        User accounts can be deleted by the user. Upon account deletion we may retain certain
+        information for legitimate business purposes or as required by law.
+    </p>
+    <h2>Do Not Track Notice</h2>
+    <p>
+        Do Not Track ("DNT") is a privacy preference that you can set in certain web browsers.
+        We do not track the users of our Site over time and across third party websites and
+        therefore do not respond to browser-initiated DNT signals.
+    </p>
+    <h2>Cookie Policy</h2>
+    <p>
+        A cookie is a small file, stored on a user's hard drive by a website. Its purpose is
+        to collect data relating to the user's browsing habits. You can choose to be notified
+        each time a cookie is transmitted. You can also choose to disable cookies entirely in
+        your internet browser, but this may decrease the quality of your user experience.
+    </p>
+    <p>
+        We use cookies to remember your identity while using the site.
+    </p>
+    <h2>Modifications</h2>
+    <p>
+        This Privacy Policy may be amended from time to time in order to maintain compliance
+        with the law and to reflect any changes to our data collection process. When we amend
+        this Privacy Policy we will update the "Effective Date" at the top of this Privacy
+        Policy. We recommend that our users periodically review our Privacy Policy to ensure
+        that they are notified of any updates. If necessary, we may notify users by email of
+        changes to this Privacy Policy.
+    </p>
+</div>
